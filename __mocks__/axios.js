79a87279fceabce71313138ab50b9df43e4e3c19
Zak Laughton <zlaughton@gmail.com>
2020-02-04 07:49:00 -0600

    add manual mock to throw unmocked error

---
+++ __mocks__/axios.js
@@ -0,0 +1,11 @@
+const axios = jest.genMockFromModule('axios');
+
+const throwUnmockedError = () => {
+  throw new Error(`This endpoint has been mocked, but hasn't been given a manual response`);
+};
+
+// Make all axios methods return the unmocked error
+// List of axios methods taken from README at https://github.com/axios/axios
+axios.get = axios.post = axios.request = axios.get = axios.delete = axios.head = axios.options = axios.post = axios.put = axios.patch = throwUnmockedError;
+
+module.exports = axios;
--- index_mocked.test.js
+++ index_mocked.test.js
@@ -4,20 +4,21 @@ const axios = require('axios');
 jest.mock('axios');
 
 it('returns the title of the first album', async () => {
-  axios.get.mockResolvedValue({
-    data: [
-      {
-        userId: 1,
-        id: 1,
-        title: 'My First Album'
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: 'Album: The Sequel'
-      }
-    ]
-  });
+  /* Comment out the mock to throw the mock error */
+  // axios.get.mockResolvedValue({
+  //   data: [
+  //     {
+  //       userId: 1,
+  //       id: 1,
+  //       title: 'My First Album'
+  //     },
+  //     {
+  //       userId: 1,
+  //       id: 2,
+  //       title: 'Album: The Sequel'
+  //     }
+  //   ]
+  // });
 
   const title = await getFirstAlbumTitle();
   expect(title).toEqual('My First Album');
